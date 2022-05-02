0e2130d024507175554f2c59792df6ea1f8cc548
Arsenii Kurilov <arsenii.kurilov@gmail.com>
2022-05-03 01:35:36 +0300

    Initial implementation

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,97 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+// eslint-disable-next-line import/no-unresolved
+import {Api} from '@octokit/plugin-rest-endpoint-methods/dist-types/types'
 
-async function run(): Promise<void> {
+type ConfirmationStatus = 'confirmed' | 'cancelled' | 'timeout' | 'pending'
+
+const getConfirmationStatus = async (
+  issues: Api['rest']['issues'],
+  issueNumber: number
+): Promise<ConfirmationStatus> => {
+  const {
+    context: {repo}
+  } = github
+  const issue = await issues.get({
+    ...repo,
+    issue_number: issueNumber
+  })
+  const {
+    data: {reactions}
+  } = issue
+
+  if (reactions?.['+1']) {
+    return 'confirmed'
+  }
+
+  if (reactions?.['-1']) {
+    return 'cancelled'
+  }
+
+  return 'pending'
+}
+
+const closeIssue = async (
+  issues: Api['rest']['issues'],
+  number: number
+): Promise<void> => {
+  const {
+    context: {repo}
+  } = github
+
+  await issues.update({...repo, issue_number: number, state: 'closed'})
+
+  core.info(`Closed issue #${number}`)
+}
+
+const run = async (): Promise<void> => {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token = core.getInput('githubToken')
+    const octokit = github.getOctokit(token)
+    const {
+      context: {repo}
+    } = github
+    const {
+      rest: {issues}
+    } = octokit
+    const {GITHUB_SHA, GITHUB_RUN_ID} = process.env
+    const commitSha = GITHUB_SHA?.substring(0, 7)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const {
+      data: {number, html_url}
+    } = await issues.create({
+      ...repo,
+      title: `🌺 Confirm deployment of ${commitSha}`,
+      body: `The confirmation step has been requested by run https://github.com/humanizmu/frontend/actions/runs/${GITHUB_RUN_ID}
 
-    core.setOutput('time', new Date().toTimeString())
+Related commit ${commitSha}
+
+To confirm this step react to this issue with 👍
+To cancel this step react to the issue with 👎`
+    })
+
+    const interval = setInterval(async () => {
+      const confirmationStatus = await getConfirmationStatus(issues, number)
+
+      core.info(`Opened issue ${html_url}`)
+      core.info(`Confirm this step by reacting to the issue with 👍`)
+      core.info('To cancel this step react to the issue with 👎')
+
+      if (confirmationStatus === 'confirmed') {
+        clearInterval(interval)
+
+        core.info('Confirmed')
+
+        await closeIssue(issues, number)
+      } else if (confirmationStatus === 'cancelled') {
+        await closeIssue(issues, number)
+
+        core.info(`Closed issue #${number}`)
+        throw new Error('Cancelled')
+      }
+    }, 10000)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    core.setFailed((error as Error).message)
   }
 }
 
